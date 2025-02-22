bbebb059612022055c27e089935bdf1811550a0e
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2025-02-22 19:13:51 +0100

    chore: initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+            }
+        }
+    }
+}
+
